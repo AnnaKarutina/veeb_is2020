80fdfbf78efa2629488bd20fb53804ef3c801f46
Anna <anna.karutina@khk.ee>
2020-02-26 11:36:40 +0200

    Loodud päringu loomine, sisustamine ja käivitamine

--- app/libs/Database.php
+++ app/libs/Database.php
@@ -9,6 +9,7 @@ class Database
   private $dbname = DB_NAME;
 
   private $dbh;
+  private $stmt;
 
   public function __construct()
   {
@@ -25,5 +26,31 @@ class Database
       echo $this->error;
     }
   }
-
+  // create statement with query
+  public function query($sql){
+    $this->stmt = $this->dbh->prepare($sql);
+  }
+  // bind values
+  public function bind($param, $value, $type = null){
+    if(is_null($type)){
+      switch (true){
+        case is_int($value):
+          $type = PDO::PARAM_INT;
+          break;
+        case is_bool($value):
+          $type = PDO::PARAM_BOOL;
+          break;
+        case is_null($value):
+          $type = PDO::PARAM_NULL;
+          break;
+        default:
+          $type = PDO::PARAM_STR;
+      }
+    }
+    $this->stmt->bindValue($param, $value, $type);
+  }
+  // execute the prepared statement
+  public function execute(){
+    return $this->stmt->execute();
+  }
 }
--- public/index.php
+++ public/index.php
@@ -4,5 +4,18 @@ require_once '../app/bootstrap.php';
 $init = new Core();
 
 $db = new Database();
+//$sql = 'INSERT INTO users SET '.
+//        'name=:name, '.
+//        'email=:email, '.
+//        'pass=:pass ';
+//
+//$db->query($sql);
+//$db->bind(':name', 'Anna Karutina');
+//$db->bind(':email', 'anna@host.loc');
+//$db->bind(':pass', 'qwerty');
+//$db->execute();
+echo '<pre>';
+print_r($db);
+echo '</pre>';
 
 
